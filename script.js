5e76e768374e9aed345cd6114400a734b9ea1d80
CaiozinS2 <caioandersongoes@gmail.com>
2023-12-13 11:06:58 -0300

    Adicionado modo música e opções de música

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ const musicaFocoInput = document.querySelector('#alternar-musica')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
 const iconeStartStop = document.querySelector('.app__card-primary-butto-icon')
 const tempoNaTela = document.querySelector('#timer')
+const seletorDeMusica = document.querySelectorAll('.app__music-button')
 
 const musica = new Audio('/sons/The-Force-Theme.mp3')
 const tempoZerado = new Audio('/sons/beep.mp3')
 const tempoIniciado = new Audio('/sons/play.mp3')
 const tempoPausado = new Audio('/sons/pause.mp3')
+const musicaLofi = new Audio('/sons/Lofi.mp3')
+const musicaRock = new Audio('/sons/Rock.mp3')
+const musicaEletronica = new Audio('/sons/Eletronica.mp3')
 
 let tempoEmSegundos = 1500
 let intervaloId = null
@@ -22,11 +26,71 @@ let tipoAtual = ''
 
 musica.loop = true
 
-musicaFocoInput.addEventListener('change', () => {
-    if(musica.paused) {
+seletorDeMusica.forEach(botaoMusica => {
+    botaoMusica.addEventListener('click', () => {
+        const contexto = botaoMusica.getAttribute('data-contexto')
+        marcarBotaoAtivo(botaoMusica)
+        escolherMusica(contexto)
+    })
+})
+
+function marcarBotaoAtivo(botaoAtivo) {
+    seletorDeMusica.forEach(botao => {
+        botao.classList.remove('active')
+    })
+    botaoAtivo.classList.add('active')
+}
+
+function escolherMusica(contexto) {
+    musica.pause() 
+
+    switch (contexto) {
+        case 'lo-fi':
+            musica.src = '/sons/Lofi.mp3'
+            break
+        case 'rock':
+            musica.src = '/sons/Rock.mp3'
+            break
+        case 'eletronica':
+            musica.src = '/sons/Eletronica.mp3'
+            break
+        default:
+            escolherMusicaPadrao()
+            return
+    }
+
+    tipoAtual = contexto
+    if (musicaFocoInput.checked) {
+    musica.play()
+    } else {
+        alert('Apenas tocará a música escolhida caso ative o Modo Música!')
+    }
+}
+
+function escolherMusicaPadrao() {
+    tipoAtual = 'padrao'
+    musica.src = '/sons/The-Force-Theme.mp3'
+    if (musicaFocoInput.checked) {
         musica.play()
+    }
+}
+
+musicaFocoInput.addEventListener('change', () => {
+    const musicaSelecionada = document.querySelector('.app__music-button.active')
+
+    if (musicaFocoInput.checked) {
+        if (musicaSelecionada) {
+            const contexto = musicaSelecionada.getAttribute('data-contexto')
+            escolherMusica(contexto)
+        } else {
+            escolherMusicaPadrao()
+        }
     } else {
         musica.pause()
+        if (musicaSelecionada) {
+            musicaSelecionada.classList.remove('active')
+        }
+        escolherMusicaPadrao()
     }
 })
 
@@ -34,19 +98,19 @@ focoBotao.addEventListener('click', () => {
     tempoEmSegundos = 1500
     alterarContexto('foco')
     focoBotao.classList.add('active')
-});
+})
 
 curtoBotao.addEventListener('click', () => {
     tempoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBotao.classList.add('active')
-});
+})
 
 longoBotao.addEventListener('click', () => {
     tempoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBotao.classList.add('active')
-});
+})
 
 function alterarContexto(contexto) {
     mostrarTempo()
@@ -54,9 +118,9 @@ function alterarContexto(contexto) {
         contexto.classList.remove('active')
     })
     if (tipoAtual !== contexto) {
-        zerar();
+        zerar()
     }
-    tipoAtual = contexto;
+    tipoAtual = contexto
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -65,27 +129,27 @@ function alterarContexto(contexto) {
             Foco é a arma mais poderosa.<br>
             <strong class="app__title-strong">Que a força esteja com você.</strong>
             `
-            break;
+            break
         case "descanso-curto":
             titulo.innerHTML = `
             Descansar você deve, padawan.<br>
             <strong class="app__title-strong">Mais forte se tornará.</strong>
             `
-            break;
+            break
         case "descanso-longo":
             titulo.innerHTML = `
             Cada segundo conta.<br>
             <strong class="app__title-strong">Faça valer a pena!</strong>
             `
         default:
-            break;
+            break
     }
 }
 
 const contagemRegressiva = () => {
     if(tempoEmSegundos <= 0) {
         tempoZerado.play()
-        tempoZerado.volume = 0.2
+        tempoZerado.volume = 0.75
         zerar()
         return
     }
@@ -126,10 +190,10 @@ mostrarTempo()
 
 function pausarTempo() {
     if (document.visibilityState === 'hidden' && intervaloId) {
-        zerar();
+        zerar()
     } else if (document.visibilityState === 'visible' && !intervaloId && iniciarOuPausarBt.textContent === 'Pausar') {
-        iniciarOuPausar();
+        iniciarOuPausar()
     }
 }
 
-document.addEventListener('visibilitychange', pausarTempo);
+document.addEventListener('visibilitychange', pausarTempo)
